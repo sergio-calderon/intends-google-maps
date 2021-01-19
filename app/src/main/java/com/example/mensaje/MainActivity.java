b4c386c216a917bb6c1014ef48dabc6b0f560686
Sergio <sergiosoler29@gmail.com>
2021-01-19 11:50:12 -0500

    First commit

---
+++ app/src/main/java/com/example/mensaje/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.mensaje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSendMessage(View view){
+        EditText myText = (EditText) findViewById(R.id.mensaje);
+        String message = myText.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        startActivity(intent);
+    }
+}
